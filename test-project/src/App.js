047bfa9fcf28c947228f29ab85a8f4f307026095
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-06-21 11:23:49 +0300

    main

---
+++ test-project/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import Nav from './componants/nav';
+import Par from './componants/paragraph';
+
+
+function App() {
+  return (
+    <div>
+   <Nav />
+
+   <Par />
+    </div>
+  );
+}
+
+export default App;
---
+++ test-project/src/componants/paragraph.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+// import '../App.css';
+
+const Par=()=>{
+
+
+    // const style={color: 'red', backgroundColor: 'blue'};
+    return(   <div>
+        <img src="https://static.remove.bg/remove-bg-web/f68d607e3305b1c23820eab456f9a63968772cfc/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"/>
+        
+        <p >Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+    </div>);
+}
+export default Par;
